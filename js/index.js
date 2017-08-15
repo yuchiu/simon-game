6407a9046df78a25db14c4f245886832c1090ea1
yu <yuchiu2002@hotmail.com>
2017-08-15 18:53:18 -0400

    CSS modified

---
+++ js/index.js
@@ -0,0 +1,46 @@
+    let redBtn = document.getElementById('red');
+    let blueBtn = document.getElementById('blue');
+    let yellowBtn = document.getElementById('yellow');
+    let greenBtn = document.getElementById('green');
+    let startToggleBtn = document.getElementById('startToggle');
+    let strictBtn = document.getElementById('strict');
+    let counter = document.getElementById('counter');
+    let message = document.getElementById('message');
+    let startSound = document.getElementById('startSound');
+    let strictSound = document.getElementById('strictSound');
+    let simonSound = document.getElementById('simonSound');
+    let failSound = document.getElementById('failSound');
+
+
+    startToggleBtn.addEventListener('click', function () {
+        playSound(startSound);
+        if (gameOn === false) {
+            start();
+
+        } else if (gameOn === true) {
+            reset();
+        }
+    })
+
+    strictBtn.addEventListener('click', function () {
+        playSound(strictSound);
+        checkStrictMode();
+    })
+    greenBtn.addEventListener('click', function () {
+        playSound(simonSound);
+        userInput(1);
+    })
+
+    redBtn.addEventListener('click', function () {
+        playSound(simonSound);
+        userInput(2);
+    })
+
+    yellowBtn.addEventListener('click', function () {
+        playSound(simonSound);
+        userInput(3);
+    })
+    blueBtn.addEventListener('click', function () {
+        playSound(simonSound);
+        userInput(4);
+    })
